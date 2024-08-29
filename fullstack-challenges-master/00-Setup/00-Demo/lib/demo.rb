99fc60c9e2ce1ff5cbdda0955ac7997c969ec1bf
laid sereir <laid.sereir00@gmail.com>
2024-08-29 16:21:04 +0200

    circle_area was defined

--- fullstack-challenges-master/00-Setup/00-Demo/lib/demo.rb
+++ fullstack-challenges-master/00-Setup/00-Demo/lib/demo.rb
@@ -1,3 +1,10 @@
 def circle_area(radius)
   # TODO: Implement the `circle_area` method
+  circle_area = 3.14*radius*radius
+
+  if (radius < 0)
+    circle_area = 0
+  end
+
+  return circle_area
 end
