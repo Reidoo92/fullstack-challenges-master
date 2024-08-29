8b08667ebc5dcc8b3b6228a59e4fc155cc14b01a
laid sereir <laid.sereir00@gmail.com>
2024-08-29 16:29:36 +0200

    respect for ruby writing conventions

--- fullstack-challenges-master/00-Setup/00-Demo/lib/demo.rb
+++ fullstack-challenges-master/00-Setup/00-Demo/lib/demo.rb
@@ -1,10 +1,8 @@
 def circle_area(radius)
   # TODO: Implement the `circle_area` method
-  circle_area = 3.14*radius*radius
+  circle_area = 3.14 * radius * radius
 
-  if (radius < 0)
-    circle_area = 0
-  end
+  circle_area = 0 if radius.negative?
 
   return circle_area
 end
