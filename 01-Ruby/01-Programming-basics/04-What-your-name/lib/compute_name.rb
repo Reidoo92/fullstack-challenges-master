cb6dd7c1db6e7ec2c56bf25b04e0328734ec0551
laid sereir <laid.sereir00@gmail.com>
2024-09-03 22:32:13 +0200

    The compute_name function has been developed and the interface to be created

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
@@ -1,3 +1,4 @@
 def compute_name(first_name, middle_name, last_name)
   # TODO: return full name using string interpolation
+  "#{first_name} #{middle_name} #{last_name}"
 end
--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,7 +1,18 @@
 require_relative "compute_name"
 
 # TODO: ask for the first name
+puts "What's your first name ?"
+first_name = gets.chomp
+
 # TODO: ask for the middle name
+puts "What's your second name ?"
+middle_name = gets.chomp
+
 # TODO: ask for the last name
+puts "What's your last name ?"
+last_name = gets.chomp
+
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
+
+print "Hello, #{compute_name(first_name, middle_name, last_name)} "
