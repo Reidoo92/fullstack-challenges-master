5fdad8c83157b7aee4e5b585b7b707e4b9466deb
laid sereir <laid.sereir00@gmail.com>
2024-09-04 11:54:18 +0200

    Functions and interface have been correctly developed

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,26 @@
+STOP_MESSAGE = "I am going to work right now!"
+
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+
+  if your_message == STOP_MESSAGE || your_message == STOP_MESSAGE.upcase
+    ""
+  elsif your_message.include?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+
+  answer = coach_answer(your_message)
+  if answer == ""
+    return ""
+  elsif your_message.upcase == your_message
+    return "I can feel your motivation! #{answer}"
+  else
+    return answer
+  end
 end
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,12 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+your_message = ""
+
+while your_message != "I am going to work right now!"
+
+  your_message = gets.chomp
+
+  coach_answer_enhanced(your_message)
+
+end
