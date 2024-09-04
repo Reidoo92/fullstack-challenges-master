6613fe643791de5799df251c2ffbb1fcb04fc138
laid sereir <laid.sereir00@gmail.com>
2024-09-04 22:33:17 +0200

    The function is functional

--- 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
+++ 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
@@ -1,4 +1,10 @@
 def allowed_to_vote?(age)
   # TODO: return (not print!) a boolean stating whether `age` is old enough to vote
   # NOTE: Use an if/else statment
+
+  if age > 17
+    return true
+  else
+    return false
+  end
 end
