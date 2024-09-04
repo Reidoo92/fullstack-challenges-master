1e89118709ac4b9930ae0fe747aaf4e13e2ae0a8
laid sereir <laid.sereir00@gmail.com>
2024-09-04 14:33:49 +0200

    code enhancement

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,6 +1,14 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
 
+  if (number % 2 == 0 && !number.zero?) || same_number(number)
+    return false
+  else
+    return true
+  end
+end
+
+def same_number(number)
   same_number = number.to_s.chars.map(&:to_i)
   same = 0
   same_number.each_with_index do |num, index|
@@ -8,9 +16,6 @@ def colorful?(number)
       same += 1 if index != other_index && num == other_num
     end
   end
-  if (number % 2 == 0 && !number.zero?) || same.positive?
-    return false
-  else
-    return true
-  end
+
+  return true if same.positive?
 end
