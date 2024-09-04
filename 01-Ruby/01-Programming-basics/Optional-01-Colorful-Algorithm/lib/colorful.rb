c8dcba71f918b02e4b19eeb645f61a43c6175bf3
laid sereir <laid.sereir00@gmail.com>
2024-09-04 14:26:19 +0200

    The colorful? function works correctly

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,3 +1,16 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
+
+  same_number = number.to_s.chars.map(&:to_i)
+  same = 0
+  same_number.each_with_index do |num, index|
+    same_number.each_with_index do |other_num, other_index|
+      same += 1 if index != other_index && num == other_num
+    end
+  end
+  if (number % 2 == 0 && !number.zero?) || same.positive?
+    return false
+  else
+    return true
+  end
 end
