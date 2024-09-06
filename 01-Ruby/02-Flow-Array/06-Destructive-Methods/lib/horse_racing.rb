260453cfc19805f996f6339cd0899688c87e6d96
laid sereir <laid.sereir00@gmail.com>
2024-09-06 17:44:56 +0200

    The function has been correctly developed

--- 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
@@ -1,3 +1,10 @@
 def horse_racing_format!(race_array)
   # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
+  number_of_horses = race_array.length
+
+  race_array.reverse!
+
+  race_array.map! do |horse|
+    "#{number_of_horses - race_array.index(horse)}-#{horse}!"
+  end
 end
