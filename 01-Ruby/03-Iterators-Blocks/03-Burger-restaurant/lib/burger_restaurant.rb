4ea42ed56cf1a1a2d6edb2431301f0a6f226993f
laid sereir <laid.sereir00@gmail.com>
2024-09-08 21:14:05 +0200

    All the burgers were successfully prepared

--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
@@ -1,3 +1,5 @@
 def burger(patty, sauce, topping)
   # TODO: code the `burger` method
+  main = (block_given? ? yield(patty) : patty)
+  ["bread", main, sauce, topping, "bread"]
 end
--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/interface.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/interface.rb
@@ -2,17 +2,23 @@ require_relative "burger_restaurant"
 
 puts "💬 I'd like a burger with a bigger portion of fish, plus mayo and salad please."
 # TODO: to upgrade a portion to a bigger one, transform the fish to uppercase
-# bigger_burger =
-
+bigger_burger = burger("fish", "mayo", "salad") do |patty|
+  patty.upcase
+end
+p bigger_burger
 
 puts "💬 I'd like a juicy steak with barbecue sauce and cheddar please."
 # TODO: to make a juicy steak, replace any vowel by the sign "~"
-# juicy_burger =
+juicy_burger = burger("steak", "barbecue", "cheddar") do |patty|
+  patty.sub('ea', "~~")
+end
 
 
 puts "💬 I'd like a spicy chicken with ketchup and cheddar please."
 # TODO: to make a spicy portion, add the sign "*" before and after the string
-# spicy_burger =
+spicy_burger = burger("chicken", "ketchup", "cheddar") do |patty|
+  "*#{patty}*"
+end
 
 
 
