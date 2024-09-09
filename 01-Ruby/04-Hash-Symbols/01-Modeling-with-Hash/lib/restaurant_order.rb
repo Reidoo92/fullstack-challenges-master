900f8b246c5b3273d1f9f49318dc6d2292636ad7
laid sereir <laid.sereir00@gmail.com>
2024-09-09 20:52:37 +0200

    Calorie counting function works perfectly

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
@@ -1,3 +1,22 @@
+MENUS = {
+  "Hamburger" => 250,
+  "Cheese Burger" => 300,
+  "Veggie Burger" => 540,
+  "Vegan Burger" => 350,
+  "Sweet Potatoes" => 230,
+  "Salad" => 15,
+  "Iced Tea" => 70,
+  "Lemonade" => 90
+}
+
 def poor_calories_counter(burger, side, beverage)
   # TODO: return number of calories for this restaurant order
+  menu = [burger, side, beverage]
+  cal = 0
+
+  MENUS.each do |element, cal_element|
+    menu.each { |ingredient| cal += cal_element if element == ingredient }
+  end
+
+  return cal
 end
