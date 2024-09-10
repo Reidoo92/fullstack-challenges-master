f22d72e3cbec036b39a554b920037ba4cf8934a7
laid sereir <laid.sereir00@gmail.com>
2024-09-10 18:25:28 +0200

    The calories_counter function has been correctly developed

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
@@ -9,10 +9,27 @@ DISHES_CALORIES = {
   "Lemonade" => 90
 }
 
+MENUS_CALORIES = {
+  "Cheesy Combo" => DISHES_CALORIES["Cheese Burger"] + DISHES_CALORIES["Sweet Potatoes"] + DISHES_CALORIES["Lemonade"],
+  "Veggie Combo" => DISHES_CALORIES["Veggie Burger"] + DISHES_CALORIES["Sweet Potatoes"] + DISHES_CALORIES["Iced Tea"],
+  "Vegan Combo" => DISHES_CALORIES["Vegan Burger"] + DISHES_CALORIES["Salad"] + DISHES_CALORIES["Lemonade"]
+}
+
 def poor_calories_counter(burger, side, beverage)
   DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
 end
 
 def calories_counter(orders)
   # TODO: return number of calories for a less constrained order
+  cal = 0
+
+  MENUS_CALORIES.each do |menu, cal_menu|
+    orders.each { |order_element| cal += cal_menu if order_element == menu }
+  end
+
+  DISHES_CALORIES.each do |dishe, cal_dish|
+    orders.each { |order_element| cal += cal_dish if order_element == dishe }
+  end
+
+  return cal
 end
