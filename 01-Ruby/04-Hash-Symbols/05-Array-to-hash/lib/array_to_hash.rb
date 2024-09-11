c18d15fd644e048d434592179fa4ddefaa17377f
laid sereir <laid.sereir00@gmail.com>
2024-09-11 15:46:46 +0200

    The function has been correctly developed

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,7 +1,5 @@
 def array_to_hash(array)
   # TODO: implement the method :)
-  # index = -1
-  # array.to_h { |element| [(index += 1).to_s, element] }
   hash = {}
 
   array.each_with_index { |value, index| block_given? ? hash["key#{index}"] = value : hash[index.to_s] = value }
