7c9e46bae62053abb3a03f1f680773fa45956f88
laid sereir <laid.sereir00@gmail.com>
2024-09-11 15:50:42 +0200

    The function has been correctly developed

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,5 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  phone_number.match?(/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[\s.-]?[1-9](?:(?:[\s.-]?\d{1}){8}|\d{2}
+  (?:[\s.-]?\d{3}){2})$/)
 end
