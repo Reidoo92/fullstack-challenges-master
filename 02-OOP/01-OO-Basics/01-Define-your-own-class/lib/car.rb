8bab305a18daa7fbef0693deb22178ce93b71c7f
laid sereir <laid.sereir00@gmail.com>
2024-09-16 15:06:13 +0200

    The car class has developed

---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/car.rb
@@ -0,0 +1,11 @@
+class Car
+  def initialize(model, brand, kilometers)
+    @model = model
+    @brand = brand
+    @kilometers = kilometers
+  end
+
+  def start
+    return @start_engine = true
+  end
+end
