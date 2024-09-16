bb2c092cdb63fe9ec185820280837523f7506df3
laid sereir <laid.sereir00@gmail.com>
2024-09-16 15:44:12 +0200

    The VendingMachine class has developed

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
@@ -10,10 +10,25 @@ class VendingMachine
   def insert_coin(input_cents)
     # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
     # when the user inserts a coin?
+    @user_balance_cents += input_cents
   end
 
   def buy_snack
     # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
     # when the user pushes a button to buy a Snack?
+
+    if @user_balance_cents >= @snack_price_cents && snack_count.positive?
+      @snack_count -= 1
+      @user_balance_cents -= @snack_price_cents
+
+    elsif @snack_count <= 0
+      p "Sorry, there are no more snacks in the machine."
+
+    else
+      p "Your balance is insufficient"
+    end
   end
+
+  attr_reader :user_balance_cents, :snack_price_cents
+  attr_accessor :snack_count
 end
