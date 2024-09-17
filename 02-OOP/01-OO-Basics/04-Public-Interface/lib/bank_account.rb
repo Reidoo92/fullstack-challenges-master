62fe2cd70895c5a6b5dd1aa1d8f6c29753d69415
laid sereir <laid.sereir00@gmail.com>
2024-09-17 16:17:25 +0200

    The BankAccount and Transaction on classes were correctly developed

--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
@@ -2,6 +2,8 @@
 class DepositError < StandardError
 end
 
+require_relative("transaction")
+
 class BankAccount
   # Contract for the BankAccount class
   # - you can access full owner's name and balance, but partial IBAN
@@ -10,6 +12,8 @@ class BankAccount
   # - you can print transactions only with a password
   # - you can withdraw or deposit money
   # - You can see the balance of the account (through the balance variable)
+
+  attr_reader :name, :balance
 
   MIN_DEPOSIT = 100
 
@@ -28,25 +32,44 @@ class BankAccount
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+
+    return add_transaction(-amount)
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+
+    return add_transaction(amount)
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array!
+
+    if args[:password]
+      args[:password] == @password ? "Transactions: #{@transactions.join(', ')}" : "wrong password"
+    else
+      return "no password given"
+    end
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+
+    first_part = @iban[0, 4]
+    last_part = @iban[-3, 3]
+
+    masked_part = "*" * (@iban.length - 7)
+
+    return "#{first_part}#{masked_part}#{last_part}"
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: return a string with the account owner, the hidden iban and the balance of the account
+
+    "#{@name} #{iban} #{@balance}"
   end
 
   private
@@ -54,5 +77,10 @@ class BankAccount
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
     # TODO: update the current balance (which represents the balance of the account)
+
+    @balance += amount
+    transaction = Transaction.new(amount)
+    @transactions << transaction
+    return transaction.to_s
   end
 end
--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/transaction.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/transaction.rb
@@ -2,9 +2,15 @@
 class Transaction
   def initialize(amount)
     # Initialize amount and date of transaction
+
+    @amount = amount
+    @date_of_transaction = Time.now
   end
 
   def to_s
     # Nicely print transaction info using Time#strftime.
+
+    formatted_date = @date_of_transaction.strftime("%d-%m-%Y %H:%M:%S")
+    "Transaction: #{format('%.2f', @amount)}€ on #{formatted_date}"
   end
 end
