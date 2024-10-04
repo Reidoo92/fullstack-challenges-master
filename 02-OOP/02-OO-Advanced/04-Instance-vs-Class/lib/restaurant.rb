2b4bc23c24c5cd41050694e3bc4dc55bcff8fc92
laid sereir <laid.sereir00@gmail.com>
2024-10-04 09:40:27 +0200

    Class has been correctly implemented

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,24 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :average_rating, :city, :name
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+
+    @city = city
+    @name = name
+    @average_rating = 0
+    @ratings = []
   end
 
   # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    @ratings << new_rate
+    totale = @ratings.sum
+    @average_rating = totale / @ratings.length
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select { |restaurant| restaurant.city == city }
+  end
 end
